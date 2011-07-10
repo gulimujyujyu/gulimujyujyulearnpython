5c73ab9fe21468ecb12261e6e4bbdc1d483d0359
Gulimujyujyu <lucienzhu@gulimujyujyu.me>
2011-07-10 23:31:14 +0800

    ADD: ex1.py

---
+++ ex1.py
@@ -0,0 +1,12 @@
+
+#!/usr/local/bin/python
+# -*- utf-8 -*-
+
+print "hello world!";
+print "hello again!";
+print "I like typing this.";
+print "This is fun.";
+print 'Yay! printing.';
+print "I'd much rather you 'not'";
+print 'I "said" do not touch this.';
+
