9a524919efbe5849469a0c4b497b194d3b1e7b0f
Gulimujyujyu <lucienatlinux@gmail.com>
2011-07-11 23:38:02 +0800

    ADD: ex3.py

---
+++ ex3.py
@@ -0,0 +1,25 @@
+
+#!/usr/local/bin/python
+# -*- utf-8 -*-
+
+print "I will now count my chickens.";
+
+print "Hens", 25+30/6;
+
+print "Roosters", 100 - 25*3%4;
+
+print "Now I will count the eggs:"
+
+print 3+2+1-5+4%2-1/4+6
+
+print "Is it true tha 3+2 < 5-7?"
+print 3+2 < 5-7
+
+print "What is 3+2?", 3+2, "Then 5-1", 5-1
+print "Oh, that's is why it's false."
+
+print "How about more?"
+print "Is it greater? 3>2", 3>2
+print "Is it greater or equal? 5>=2", 5>=2
+print "Is is less or equal? 5<=2", 5<=2
+
