88ca93b6f2de11d9fb20bf947fcd638162bb7dfc
Gulimujyujyu <lucienatlinux@gmail.com>
2011-07-17 00:28:30 +0800

    MOD: ex8.py, test docstring

--- ex8.py
+++ ex8.py
@@ -1,6 +1,8 @@
 
 #!/usr/local/bin/python 
 # -*- coding:utf-8 -*-
+import re
+
 formatter = "%r %r %r %r"
 
 print formatter % (1, 2, 3, 4)
@@ -14,3 +16,19 @@ print formatter % (
 	"But it didn't sing.",
 	"So I said good night."
 )
+
+LIGHT_MESSAGES = {
+    'English': "There are %(number_of_lights)s lights.",
+    'Pirate':  "Arr! Thar be %(number_of_lights)s lights."
+}
+
+def lights_message(language, number_of_lights):
+    """Return a language-appropriate string reporting the light count."""
+    return LIGHT_MESSAGES[language] % locals()
+
+def is_pirate(message):
+    """Return True if the given message sounds piratical."""
+    return re.search(r"(?i)(arr|avast|yohoho)!", message) is not None
+
+print lights_message('English',12)
+print is_pirate("Hello")
