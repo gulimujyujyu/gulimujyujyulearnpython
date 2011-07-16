e765ad5ba8c3c7effaa3191103b0fdbf0fa18117
Gulimujyujyu <lucienatlinux@gmail.com>
2011-07-17 00:34:21 +0800

    ADD: ex9.py

---
+++ ex9.py
@@ -0,0 +1,25 @@
+
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
+# here are some new strange stuff, remeber type it exactly
+
+days = "Mon Tue Web Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+months_single_quote = 'Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec'
+
+print "Here are days:", days
+print "Here are months:", months
+print "Here are months:", months_single_quote
+
+print '''
+There's something going on here,
+With the three single-quotes,
+We'll be able to type as much as we like.
+'''
+
+print """
+There's something going on here,
+With the three double-quotes,
+We'll also be able to type as much as we like.
+"""
