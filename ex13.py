364762ce44c1bba332d82e20b34a911fd2c7d0c5
Gulimujyujyu <lucienatlinux@gmail.com>
2011-07-19 10:28:53 +0800

    ADD: ex13.py

---
+++ ex13.py
@@ -0,0 +1,12 @@
+
+#!/usr/local/bin/python
+# -*- coding:utf-8 -*-
+from sys import argv
+
+#assignment by orders!
+#but only four elements are well unpacked, not more or not less.
+script, first, second, third = argv
+print "The script is called: ", script
+print "First arguement is: ", first
+print "Second arguement is: ", second
+print "Third arguement is: ", third
