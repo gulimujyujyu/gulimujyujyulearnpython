f2ff0b23c2743c462c30242cc888b1e51301bea0
Gulimujyujyu <lucienatlinux@gmail.com>
2011-07-25 22:43:35 +0800

    ADD: ex22.py

---
+++ ex22.py
@@ -0,0 +1,21 @@
+
+#!/usr/local/bin/python
+# -*- coding:utf-8 -*-
+#make a list
+
+#better use '''...'''
+print "My List 0-21"
+print "=============================="
+print "print"
+print "+ - * / % > <"
+print "string formatter"
+print "raw_input('>')"
+print "from sys import argv"
+print "unpacking"
+print "open() return a file object"
+print "somefile.read()"
+print "somefile.truncate()"
+print "somefile.write()"
+print "somefile.close()"
+print "rewind(somefile)"
+print "somefile.readline()"
