4498395be79d95420db087c41a3766ac49c67089
Gulimujyujyu <lucienatlinux@gmail.com>
2011-07-30 01:43:08 +0800

    ADD: ex33.py

---
+++ ex33.py
@@ -0,0 +1,20 @@
+
+#!/usr/local/bin/python
+# -*- coding:utf-8 -*-
+i = 0
+numbers = []
+
+while i < 6:
+	print "At the top, i is %d" % i
+	numbers.append(i)
+	
+	i = i+1
+	print "Numbers now: ", numbers
+	print "At the bottom, i is %d" % i
+	
+
+print "The numbers: ", numbers
+
+for num in numbers:
+	print num
+	
