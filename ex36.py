e90e036f5d81fb618b61ddf7231b6c9fb0c4bf31
Xiaolong ZHU <lucienzhu@gmail.com>
2011-08-08 22:53:28 +0800

    ADD: ex36.py

---
+++ ex36.py
@@ -0,0 +1,15 @@
+﻿
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
+print 'Use die function!'
+useDie = True
+
+def die():
+	print 'Dying'
+	exit(0)
+
+if useDie:
+	die();
+else:
+	print 'Do not use die.'
