6e111b4d21efa32ad7f1c264f482fcd9fae53348
Xiaolong ZHU <lucienzhu@gmail.com>
2011-08-09 21:58:53 +0800

    ADD: ex37-39.py

---
+++ ex37.py
@@ -0,0 +1,18 @@
+﻿
+#!/usr/local/bin/python
+# -*- coding:utf-8 -*-
+
+print "learning keywords"
+print "yet to know: yeild, exec, lambda"
+
+print "learning data types"
+
+print "learning string escapes"
+print "1\a;2\b;3\v;4\f"
+print "\\a;\\b;\\v;\\f"
+
+print "learning string formats"
+print "diouxXeEfFgGcrs"
+
+print "learning operators"
+print "%r, %r, %r" %( 3**2, 10//4, 10%3)
---
+++ ex38.py
@@ -0,0 +1,5 @@
+﻿
+#!/usr/local/bin/python
+# -*- coding:utf-8 -*-
+
+#NOTE: code review
